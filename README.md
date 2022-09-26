f3c1f563e9eb5216f203ed01f290ac74ad041241
ilies ouldmenouer <ilyasdzair1@gmail.com>
2022-09-27 00:33:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# iHearYourSign
+ iHearYourSign - A real-time sign recognition , that detect arabic hand sign join all characters and speak it as a single word not characters the person can use x sign for space and overtime system detect sign a beep sound while play to move into next sign and so on  It allow def people to be heard 
---
+++ main.py
@@ -0,0 +1,33 @@
+print("protoype hand sign speaker")
+
+from gtts import gTTS
+import os
+
+
+mytext = 'مرحبا'
+
+textCharachter = []
+os.system("mpg321 beep.mp3")
+
+
+def speak(text):
+    language = 'ar'
+    myobj = gTTS(text=text, lang=language, slow=False)
+    myobj.save("soundFileHolder.mp3")
+    # Playing the converted file
+    os.system("mpg321 soundFileHolder.mp3")
+
+
+def read():
+    val = input("Reading: ")
+    if val == "read":
+        print(textCharachter)
+        print(''.join(textCharachter))
+        speak(''.join(textCharachter))
+    else:
+        textCharachter.append(val)
+        os.system("mpg321 beep.mp3")
+        read()
+
+read()
+
