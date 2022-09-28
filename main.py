05ab37618253ed5684f6d8d9352f779276e19f59
ilies ouldmenouer <ilyasdzair1@gmail.com>
2022-09-28 12:02:34 +0100

    detect hand and draw landmarks

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-print("protoype hand sign speaker")
+import cv2
+import imutils as imutils
+import mediapipe as mp
 
-from gtts import gTTS
-import os
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                     max_num_hands=1,
+                     min_detection_confidence=0.5,
+                     min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    img = imutils.resize(img, width=400)
+    winName = "Image"
+    cv2.namedWindow(winName)  # Create a named window
+    cv2.moveWindow(winName, 800, 400)  # Move it to (40,30)
 
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
 
-mytext = 'مرحبا'
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-textCharachter = []
-os.system("mpg321 beep.mp3")
-
-
-def speak(text):
-    language = 'ar'
-    myobj = gTTS(text=text, lang=language, slow=False)
-    myobj.save("soundFileHolder.mp3")
-    # Playing the converted file
-    os.system("mpg321 soundFileHolder.mp3")
-
-
-def read():
-    val = input("Reading: ")
-    if val == "read":
-        print(textCharachter)
-        print(''.join(textCharachter))
-        speak(''.join(textCharachter))
-    else:
-        textCharachter.append(val)
-        os.system("mpg321 beep.mp3")
-        read()
-
-read()
-
+    cv2.imshow(winName, img)
+    cv2.waitKey(1)
